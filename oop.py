7b4d73abb12fd3bcffc60bb2066a4e798e039620
Stephaniennesochi <ezestephanie64@gmail.com>
2024-03-01 23:40:29 +0100

    oop

---
+++ oop.py
@@ -0,0 +1,22 @@
+class laptop():
+    def display(self):
+        print("I type works efficiently")
+        print("I solve problems")
+        
+class phone():
+    def display(self):
+        print("I do mini jobs")
+        print("I am portable") 
+               
+class ipad():
+    def display(self):
+        print("I encourage game play")
+        print("I am lightweight")
+        
+object_laptop = laptop()
+object_phone = phone()
+object_ipad = ipad()
+
+object_laptop.display()
+object_phone.display()
+object_ipad.display()
---
+++ poly.py
@@ -0,0 +1,36 @@
+class spagetti():
+    def order(self):
+        print("Pasta is the best")
+        print("I solve problems")
+        
+class indomie():
+    def order(self):
+        print("It saves time and it is faster")
+        print("It is easy to prepare") 
+               
+class soup():
+    def order(self):
+        print("It is healthy")
+        print("It fulls the tummy")
+        
+class rice():
+    def order(self):
+        print("I LOVE RICE")
+        print("THEY ARE MY FAVOURITE")
+
+class beans():
+    def order(self):
+        print("BEANS ARE PROTEINOUS")
+        print("THEY ENHANCE GROWTH")
+        
+object_spagetti = spagetti()
+object_indomie = indomie()
+object_soup = soup()
+object_rice = rice()
+object_beans = beans()
+
+object_spagetti.order()
+object_indomie.order()
+object_soup.order()
+object_rice.order()
+object_beans.order()
